2b74ea37591876229db0660e75aaf016335dc636
JT <hellares98@gmail.com>
2026-04-12 17:52:44 -0500

    feat: syncrofact deploy stack

---
+++ mysql-init/01-create-databases.sql
@@ -0,0 +1,3 @@
+-- Crear bases de datos para ambos ambientes
+CREATE DATABASE IF NOT EXISTS facturacion_beta CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS facturacion_prod CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
